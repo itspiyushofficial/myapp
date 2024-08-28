a246053c558d48654fb97ce4678f1a0c7de74258
itspiyushofficial <piyushgupta46bte@gmail.com>
2024-08-28 12:30:11 +0530

    first file

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Service from "./Service";
+import Contact from "./Contact";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" Component={Home} />
+        <Route exact path="/about" Component={About} />
+        <Route exact path="/service" Component={Service} />
+        <Route exact path="/contact" Component={Contact} />
+        {/* <Redirect to="/" /> */}
+      </Routes>
+      <Footer/>
+    </>
+  );
+};
+
+export default App;
---
+++ src/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import web from "../src/Images/img2.png";
+import { NavLink } from "react-router-dom";
+import Common from "./Common";
+
+const Home = () => {
+  return (
+    <>
+      <Common
+      name="Grow your Business with " 
+      imgsrc={web} 
+      visit="/service" 
+      btname="Get Started" 
+      />
+    </>
+  );
+};
+
+export default Home;
